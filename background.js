e8be9b52ff2761ed540e7d7ffe1498dea3cd9edb
MateusdeLima <silvamateusdelima@yahoo.com.br>
2024-12-19 14:38:30 -0300

    first commit

---
+++ background.js
@@ -0,0 +1,56 @@
+let openTabs = {};
+let blockedTabs = new Set();
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url) {
+    let url = new URL(changeInfo.url);
+    let productId = url.pathname.match(/i\.\d+\.\d+/);
+
+    if (productId) {
+      productId = productId[0];
+
+      if (blockedTabs.has(productId)) {
+        chrome.tabs.remove(tabId);
+      } else if (openTabs[productId]) {
+        chrome.tabs.remove(tabId);
+      } else {
+        openTabs[productId] = { id: tabId, url: changeInfo.url };
+        chrome.storage.local.get({ history: [] }, data => {
+          let history = data.history;
+          history.push(changeInfo.url);
+          chrome.storage.local.set({ history });
+        });
+      }
+      chrome.storage.local.set({ openTabs });
+    }
+  }
+});
+
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+  for (let productId in openTabs) {
+    if (openTabs[productId].id === tabId) {
+      blockedTabs.add(productId);
+      delete openTabs[productId];
+      chrome.storage.local.set({ openTabs });
+      break;
+    }
+  }
+});
+
+function resetOpenTabs() {
+  openTabs = {};
+  blockedTabs.clear();
+  chrome.storage.local.set({ openTabs, history: [] });
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "reset") {
+    resetOpenTabs();
+    sendResponse({ status: "reset" });
+  } else if (request.action === "getOpenTabs") {
+    chrome.storage.local.get("history", data => {
+      sendResponse({ history: data.history });
+    });
+    return true; // keep the message channel open for sendResponse
+  }
+});
---
+++ popup.js
@@ -0,0 +1,31 @@
+document.getElementById("reset").addEventListener("click", () => {
+    const resetButton = document.getElementById("reset");
+    resetButton.disabled = true;
+    resetButton.textContent = "Resetting...";
+  
+    chrome.runtime.sendMessage({ action: "reset" }, response => {
+      if (response.status === "reset") {
+        setTimeout(() => {
+          resetButton.disabled = false;
+          resetButton.textContent = "Reset Open Tabs";
+          displayOpenTabs();
+        }, 2000);
+      }
+    });
+  });
+  
+  function displayOpenTabs() {
+    chrome.runtime.sendMessage({ action: "getOpenTabs" }, response => {
+      const openTabsList = document.getElementById("openTabsList");
+      openTabsList.value = "";
+  
+      if (response.history) {
+        response.history.forEach(url => {
+          openTabsList.value += url + "\n";
+        });
+      }
+    });
+  }
+  
+  document.addEventListener("DOMContentLoaded", displayOpenTabs);
+  
